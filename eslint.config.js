f09f7b9577d80fc2ceeb683de9443e9e2a411f25
TriplEight <pisarevden@gmail.com>
2025-12-15 21:46:21 +0100

    medium prio issues

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: eslintPluginAstro.parser,
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+    rules: {
+      'astro/no-set-html-directive': 'warn',
+      'astro/no-unused-define-vars-in-style': 'warn',
+    },
+  },
+  {
+    ignores: [
+      'dist/',
+      'node_modules/',
+      '.astro/',
+      '*.config.{js,mjs,cjs}',
+      'public/',
+    ],
+  },
+];
